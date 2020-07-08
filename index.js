e9aa9c5f62e4d27c7910e54f2c117bd2c94f9ff2
Lief Friedrichs <lief.friedrichs@gmail.com>
2020-07-08 19:41:09 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,19 +1,23 @@
 function buildHistogram(array) {
   const histogram = {}
+  array.forEach
   return histogram
 }
 
 function canBuildNote(array, note) {
   const histogram = buildHistogram(array);
-  const noteContent = buildHistogram(note.split(''));
+  const noteContent = buildHistogram(note.split('').filter(letter => letter !== ' '));
+  let answer = true;
   for (var key in noteContent) {
     if (histogram[key]) {
       if (histogram[key] === 0) {
-        return false
+        asnwer = false
       } else {
         histogram[key] -= 1;
       }
+    } else {
+      answer = false
     }
   }
-  return histogram
+  return answer
 }
